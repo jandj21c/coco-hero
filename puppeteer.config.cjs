c6fe996ac38b97f027f33f5583dfb48540baadec
timothy.kim <timothy.kim@kakaocorp.com>
2024-12-28 03:36:52 +0900

    123

--- puppeteer.config.cjs
+++
@@ -1,6 +0,0 @@
-// render 가 chromium 을 설치하게 한다
-const { join } = require('path');
-/**
-* @type {import("puppeteer").Configuration}
-*/
-module.exports = { cacheDirectory: join(__dirname, '.cache', 'puppeteer') };
